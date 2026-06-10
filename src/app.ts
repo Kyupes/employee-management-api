03c4a12053981c5b3281fe1182dc21c2d6b4b20a
Igor Gissi <gissiigor@gmail.com>
2026-06-10 09:56:41 -0300

    feat: creating new error infrastructure, not implemented yet

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import employeeRoutes from "./routes/employees";
+import { globalErrorHandler } from "./middlewares/errorHandler";
 
 export const app = express();
 
 app.use(express.json());
 app.use("/", employeeRoutes);
+app.use(globalErrorHandler);
---
+++ src/middlewares/errorHandler.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+import { AppError } from '../errors/appError';
+
+export const globalErrorHandler = (
+    err: AppError,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    let statusCode = 500;
+    let message = "Internal server Error";
+    if ( err instanceof AppError ){
+        statusCode = err.statusCode;
+        message = err.message;
+    }
+    res.status(statusCode).json({ 
+        status: 'Error',
+        statusCode,
+        message,
+     });
+};
