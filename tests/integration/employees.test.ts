43390ef030a1ff051d97baeeb9e8155b7e500882
Igor Gissi <gissiigor@gmail.com>
2026-07-08 10:58:51 -0300

    feat: added more integration tests for put employees endpoint and added delete employees endpoint testing

--- tests/integration/employees.test.ts
+++ tests/integration/employees.test.ts
@@ -169,5 +169,75 @@ describe('Employees API', () => {
             expect(response.body).toHaveProperty('message', 'Validation failed');
             expect(response.body).toHaveProperty('errors');
         });
+
+        it('should return 404 when user try to update another users employee', async () => {
+            const authRequest = authenticatedRequest(testUser.token);
+            const response = await authRequest.put('/employees/3').send(testEmployees.testEmployeeCorrect);
+            expect(response.status).toBe(404);
+            expect(response.body).toHaveProperty('status', 'Error');
+            expect(response.body).toHaveProperty('statusCode', 404);
+            expect(response.body).toHaveProperty('message', 'Employee not found');
+        });
+
+        it('should update and return employee', async () => {
+            const authRequest = authenticatedRequest(testUser.token);
+            const response = await authRequest.put('/employees/1').send(testEmployees.testEmployeeCorrect);
+            expect(response.status).toBe(200);
+            expect(response.body).toHaveProperty('name', 'Robert Williams');
+            expect(response.body).toHaveProperty('role', 'Tech Lead');
+        });
+    });
+
+    describe('DELETE /employees/:id', () => {
+        it('should return 401 without authentication', async () => {
+            const response = await request(app).delete('/employees/1');
+            expect(response.status).toBe(401);
+            expect(response.body).toHaveProperty('status', 'Error');
+            expect(response.body).toHaveProperty('statusCode', 401);
+            expect(response.body).toHaveProperty('message', 'Authentication required');
+        });
+
+        it('should return 403 for user not authorized', async () => {
+            const authRequest = authenticatedRequest(testUser.token);
+            const response = await authRequest.delete('/employees/1');
+            expect(response.status).toBe(403);
+            expect(response.body).toHaveProperty('status', 'Error');
+            expect(response.body).toHaveProperty('statusCode', 403);
+            expect(response.body).toHaveProperty('message', 'Insufficient permissions');
+        });
+
+        it('should successfuly delete employee by admin', async () => {
+            const authRequest = authenticatedRequest(adminUser.token);
+            const response = await authRequest.delete('/employees/1');
+            expect(response.status).toBe(204);
+            expect(response.body).not.toHaveProperty('status');
+        });
+
+        it('should return 403 for unauthorized access by user', async () => {
+            const authRequest = authenticatedRequest(testUser.token);
+            const response = await authRequest.delete('/employees/3');
+            expect(response.status).toBe(403);
+            expect(response.body).toHaveProperty('status', 'Error');
+            expect(response.body).toHaveProperty('statusCode', 403);
+            expect(response.body).toHaveProperty('message', 'Insufficient permissions');
+        });
+
+        it('should return 403 for non-existent employee by user', async () => {
+            const authRequest = authenticatedRequest(testUser.token);
+            const response = await authRequest.delete('/employees/10');
+            expect(response.status).toBe(403);
+            expect(response.body).toHaveProperty('status', 'Error');
+            expect(response.body).toHaveProperty('statusCode', 403);
+            expect(response.body).toHaveProperty('message', 'Insufficient permissions');
+        });
+
+        it('should return 404 for non-existent employee by admin', async () => {
+            const authRequest = authenticatedRequest(adminUser.token);
+            const response = await authRequest.delete('/employees/10');
+            expect(response.status).toBe(404);
+            expect(response.body).toHaveProperty('status', 'Error');
+            expect(response.body).toHaveProperty('statusCode', 404);
+            expect(response.body).toHaveProperty('message', 'Employee not found');
+        });
     });
 });
