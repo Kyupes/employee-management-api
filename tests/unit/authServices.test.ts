c3550f71ffd04c28bc71095c8128a07a6529bf68
Igor Gissi <gissiigor@gmail.com>
2026-07-09 10:34:02 -0300

    refactor: same restructuration for unit auth testing

--- tests/unit/authServices.test.ts
+++ tests/unit/authServices.test.ts
@@ -3,6 +3,7 @@ import * as authServices from '../../src/services/authServices';
 import * as repository from '../../src/repository/authRepository';
 import { AppError } from '../../src/errors/appError';
 import { comparePassword, hashPassword } from '../../src/utils/password';
+import { UserRole } from '../../src/types/userInterfaces';
 
 vi.mock('../../src/repository/authRepository');
 vi.mock('../../src/utils/password');
@@ -13,20 +14,26 @@ describe('authServices', () => {
     });
 
     const userData =  { email: 'test@test.com', password: 'SecurePass123' };
+    const mockUser = {
+        id: 1,
+        email: 'test@test.com',
+        password_hash: '$2a$10$fakehash',
+        created_at: '2024-01-01',
+        role: 'user' as UserRole,
+    };
 
     describe('registerUser', () => {
         it('should create a user with hashed password', async () => {
             vi.mocked(repository.findByEmail).mockResolvedValue(null);
-            vi.mocked(repository.create).mockResolvedValue({
-                id: 1,
-                email: 'test@test.com',
-                password_hash: '$2a$10$fakehash',
-                created_at: '2024-01-01',
-                role: 'user'
-            });
+            vi.mocked(repository.create).mockResolvedValue(mockUser);
             const result = await authServices.registerUser(userData);
-            expect(result).toHaveProperty('id');
-            expect(result).toHaveProperty('email', 'test@test.com');
+            expect(result).toEqual(
+                expect.objectContaining({
+                    id: mockUser.id,
+                    email: userData.email,
+                    created_at: mockUser.created_at,
+                })
+            );
             expect(result).not.toHaveProperty('password_hash');
             expect(hashPassword).toHaveBeenCalled();
             expect(repository.create).toHaveBeenCalledWith(
@@ -37,15 +44,13 @@ describe('authServices', () => {
         });
 
         it('should throw 409 if email already exists', async () => {
-            vi.mocked(repository.findByEmail).mockResolvedValue({
-                id: 1,
-                email: 'test@test.com',
-                password_hash: '$2a$10$fakehash',
-                created_at: '2024-01-01',
-                role: 'user'
+            vi.mocked(repository.findByEmail).mockResolvedValue(mockUser);
+            await expect(authServices.registerUser(userData)).rejects.toBeInstanceOf(AppError);
+            await expect(authServices.registerUser(userData))
+            .rejects.toMatchObject({
+                message: 'Email already in use',
+                statusCode: 409,
             });
-            await expect(authServices.registerUser(userData)).rejects.toThrow();
-            await expect(authServices.registerUser(userData)).rejects.toThrow(new AppError('Email already in use', 409));
             expect(hashPassword).not.toHaveBeenCalled();
             expect(repository.create).not.toHaveBeenCalled();
         });
@@ -53,37 +58,39 @@ describe('authServices', () => {
     
     describe('loginUser', () => {
         it('should return a token for valid credentials', async () => {
-            vi.mocked(repository.findByEmail).mockResolvedValue({
-                id: 1,
-                email: 'test@test.com',
-                password_hash: '$2a$10$fakehash',
-                created_at: '2024-01-01',
-                role: 'user'
-            })
+            vi.mocked(repository.findByEmail).mockResolvedValue(mockUser)
             vi.mocked(comparePassword).mockResolvedValue(true);
             const result = await authServices.loginUser(userData);
             expect(result).toHaveProperty('token');
-            expect(result.token).toMatch(/^eyJ/);
-            expect(result.user).toHaveProperty('email', 'test@test.com');
+            expect(result).toEqual(
+                expect.objectContaining({
+                    token: expect.stringMatching(/^eyJ/),
+                    user: expect.objectContaining({ email: userData.email }),
+                })
+            );
             expect(result.user).not.toHaveProperty('password_hash');
         });
 
         it('should throw 401 for invalid credentials for inexistent user', async () => {
             vi.mocked(repository.findByEmail).mockResolvedValue(null);
-            await expect(authServices.loginUser(userData)).rejects.toThrow(new AppError('Invalid credentials', 401));
+            await expect(authServices.loginUser(userData)).rejects.toBeInstanceOf(AppError);
+            await expect(authServices.loginUser(userData))
+            .rejects.toMatchObject({
+                message: 'Invalid credentials',
+                statusCode: 401,
+            });
             expect(comparePassword).not.toHaveBeenCalled();
         });
 
         it('should throw 401 for invalid credentials for invalid password', async () => {
-            vi.mocked(repository.findByEmail).mockResolvedValue({
-                id: 1,
-                email: 'test@test.com',
-                password_hash: '$2a$10$fakehash',
-                created_at: '2024-01-01',
-                role: 'user'
-            });
+            vi.mocked(repository.findByEmail).mockResolvedValue(mockUser);
             vi.mocked(comparePassword).mockResolvedValue(false);
-            await expect(authServices.loginUser(userData)).rejects.toThrow(new AppError('Invalid credentials', 401));
+            await expect(authServices.loginUser(userData)).rejects.toBeInstanceOf(AppError);
+            await expect(authServices.loginUser(userData))
+            .rejects.toMatchObject({
+                message: 'Invalid credentials',
+                statusCode: 401,
+            });
         });
     });
 });
