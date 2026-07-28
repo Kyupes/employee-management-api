f7300d48353726a1e84931bee25ef6fadea1c0e8
Igor Gissi <gissiigor@gmail.com>
2026-07-28 10:16:37 -0300

    refactor: schemas now use zod extend to prevent repeating logic

--- src/schemas/employee.schema.ts
+++ src/schemas/employee.schema.ts
@@ -1,19 +1,49 @@
 import { z } from 'zod';
+import { extendZodWithOpenApi } from '@asteasolutions/zod-to-openapi';
 
-export const createEmployeeSchema = z.object({
-    name: z.string().min(5),
-    role: z.string(),
-    salary: z.number().min(1000),
-    active: z.boolean()
+extendZodWithOpenApi(z);
+
+const baseEmployeeSchema = z.object({
+    name: z.string().min(5).openapi({
+        description: 'Full name of the employee',
+        example: 'John Doe',
+    }),
+    role: z.string().openapi({ 
+        description: 'Job title or role',
+        example: 'Software Engineer',
+    }),
+    salary: z.number().min(1000).openapi({ 
+        description: 'Monthly salary in USD',
+        example: 5000,
+    }),
+    active: z.boolean().openapi({ 
+        description: 'Wheter the employee is currently active',
+        example: true,
+    }),
+});
+
+export const createEmployeeSchema = baseEmployeeSchema
+.openapi('CreateEmployeeRequest', { 
+    description: 'Request payload to create a new employee'
 });
 export type CreateEmployeeInput = z.infer<typeof createEmployeeSchema>;
 
-export const updateEmployeeSchema = z.object({
-    name: z.string().min(5),
-    role: z.string(),
-    salary: z.number().min(1000),
-    active: z.boolean()
+
+export const employeeResponseSchema = baseEmployeeSchema.extend({
+    id: z.number().positive().openapi({
+        description: 'Unique identifier for employee',
+        example: 48235,
+    }),
+    userId: z.number().positive().openapi({
+        description: 'Unique identifier for user owner',
+        example: 732,
+    }),
+}).openapi('EmployeeResponse', {
+    description: 'Response for employee, returned upon successful creation, retrieval or update operations.'
 });
+
+
+export const updateEmployeeSchema = baseEmployeeSchema;
 export type UpdateEmployeeInput = z.infer<typeof updateEmployeeSchema>;
 
 export const employeeIdParamSchema = z.object({
