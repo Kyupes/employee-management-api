a820c49cc8a76e0b85183f39392610fc8629ee42
Igor Gissi <gissiigor@gmail.com>
2026-07-28 11:06:02 -0300

    refactor: changed schemas name to use same pattern across application, restructured to use extend and added response schemas for later documentation

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import * as controllers from "../controllers/authController";
 import { validate } from "../middlewares/validate";
-import { UserRegistrySchema, UserLoginSchema } from "../schemas/auth.schema";
+import { userRegistrySchema, userLoginSchema } from "../schemas/auth.schema";
 
 const router = express.Router();
 
-router.post("/auth/register", validate(UserRegistrySchema, 'body'), controllers.createUser);
-router.post("/auth/login", validate(UserLoginSchema, 'body'), controllers.login);
+router.post("/auth/register", validate(userRegistrySchema, 'body'), controllers.createUser);
+router.post("/auth/login", validate(userLoginSchema, 'body'), controllers.login);
 
 export default router;
--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -1,16 +1,47 @@
 import { z } from "zod";
 
-export const UserRegistrySchema = z.object({
-    email: z.email("Invald email format"),
+const baseUserSchema = z.object({
+    email: z.email("Invald email format").openapi({
+        description: 'Unique user email',
+        example: 'testUser@test.com',
+    }),
+});
+
+export const userRegistrySchema = baseUserSchema.extend({
     password: z.string()
     .min(8, "Must contain at least 8 characters")
     .regex(/[a-zA-Z]/, "Must contain at least one letter")
-    .regex(/[0-9]/, "Must contain at least one number"),
+    .regex(/[0-9]/, "Must contain at least one number")
+    .openapi({
+        description: 'String user password before hashing',
+        example: 'securePass123',
+    }),
 });
-export type UserRegistryInput = z.infer<typeof UserRegistrySchema>;
+export type UserRegistryInput = z.infer<typeof userRegistrySchema>;
 
-export const UserLoginSchema = z.object({
-    email: z.email("Invalid email format"),
-    password: z.string(),
+export const userRegistryResponse = baseUserSchema.extend({
+    id: z.number().positive().openapi({
+        description: 'User unique integer identifier',
+        example: 1247,
+    }),
+    created_at: z.string().openapi({
+        description: 'Data of user account creation',
+        example: '2026-07-22T13:48:21.399Z',
+    }),
 });
-export type UserLoginInput = z.infer<typeof UserLoginSchema>;
+
+export const userLoginSchema = baseUserSchema.extend({
+    password: z.string().openapi({
+        description: 'User login password',
+        example: 'securePass123',
+    }),
+});
+export type UserLoginInput = z.infer<typeof userLoginSchema>;
+
+export const userLoginResponse = z.object({
+    token: z.string().openapi({
+        description: 'User token on successful login',
+        example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQ..."
+    }),
+    user: baseUserSchema,
+})
