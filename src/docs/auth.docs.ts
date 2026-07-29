dfac5c9b8bd34c231565129f9946b3ede324bc02
Igor Gissi <gissiigor@gmail.com>
2026-07-29 10:05:00 -0300

    added register path documentation

--- src/docs/auth.docs.ts
+++ src/docs/auth.docs.ts
@@ -44,3 +44,48 @@ registry.registerPath({
         },
     },
 });
+
+
+registry.registerPath({
+    method: 'post',
+    path: '/auth/register',
+    summary: 'Register new user',
+
+    security: [],
+    request: {
+        body: {
+            content: {
+                'application/json': {
+                    schema: userRegistrySchema,
+                },
+            },
+        },
+    },
+    responses: {
+        201: {
+            description: 'Successfull user creation',
+            content: {
+                'application/json': {
+                    schema: userRegistryResponse,
+                },
+            },
+        },
+        400: {
+            description: 'Validation error or malformed request',
+            content: {
+                'application/json': {
+                    schema: errorResponseSchema,
+                },
+            },
+        },
+        409: {
+            description: 'Email provided is already used',
+            content: {
+                'application/json': {
+                    schema: errorResponseSchema,
+                },
+            },
+        },
+        
+    },
+});
