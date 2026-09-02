7e67a94502ad2128aa4372efab258f4fb51e4544
Igor Gissi <gissiigor@gmail.com>
2026-09-02 11:17:00 -0300

    added cache service with get, set and remove functions for redis cache management

---
+++ src/cache/cacheService.ts
@@ -0,0 +1,35 @@
+import { redisClient } from "../config/redis";
+
+export async function get<T>(key: string): Promise<T | undefined>{
+    try {
+        const value = await redisClient.get(key);
+        if (value != null) {
+            const result = JSON.parse(value);
+            return result;
+        }
+        return undefined;
+    } catch (err) {
+        if (err instanceof SyntaxError){
+            console.error("JSON parsing failed:", err);
+        }
+        else {
+            console.error("Redis get error:", err);
+        }
+    }
+}
+
+export async function set<T>(key: string, value: T, ttl: number): Promise<void>{
+    try {
+        await redisClient.set(key, JSON.stringify(value), { expiration: { type: "EX", value: ttl } });
+    } catch (err) {
+        console.error("Redis set error:", err);
+    }
+}
+
+export async function remove(key: string): Promise<void>{
+    try{
+        await redisClient.del(key);
+    } catch (err) {
+        console.error("Redis delete error:", err);
+    }
+}
