9965646e00a5fa687e86135a230adfea4c91d797
Igor Gissi <gissiigor@gmail.com>
2026-09-03 11:36:25 -0300

    added versiong handling module to fit a better architecture

---
+++ src/cache/cacheVersionService.ts
@@ -0,0 +1,20 @@
+import { redisClient } from "../config/redis";
+
+
+export async function initializeVersion(key: string): Promise<void>{
+    try {
+        await redisClient.setNX(key, '1');
+    } catch (err) {
+        console.error("Redis version initialization error:", err);
+    }
+}
+
+export async function incrementVersion(key: string): Promise<number | null>{
+    try {
+        const version = await redisClient.incr(key);
+        return version;
+    } catch (err) {
+        console.error("Redis increment version error:", err);
+        return null;
+    }
+}
