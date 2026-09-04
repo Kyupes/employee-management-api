816361705b496e3357b5616f70fa49fee4205377
Igor Gissi <gissiigor@gmail.com>
2026-09-04 11:45:57 -0300

    added specialized get version function with structure state handling

--- src/cache/cacheVersionService.ts
+++ src/cache/cacheVersionService.ts
@@ -1,5 +1,34 @@
 import { redisClient } from "../config/redisClient";
 
+export type VersionResult =
+    | { 
+        status: "found";
+        version: number;
+    }
+    | {
+        status: "missing";
+    }
+    | {
+        status: "error";
+    };
+
+export async function getVersion(key: string): Promise<VersionResult>{
+    try {
+        const version = await redisClient.get(key);
+        if (version === null){
+            return { status: "missing" };
+        }
+        const versionNumber = Number(version);
+        if ( Number.isNaN(versionNumber) || versionNumber <= 0 ){
+            return { status: "error" };
+        }
+        return { status: "found", version: versionNumber };
+    } catch (err) {
+        console.error("Redis get version error:", err);
+        return { status: "error" };
+    }
+}
+
 export async function initializeVersion(key: string): Promise<void>{
     try {
         await redisClient.setNX(key, '1');
