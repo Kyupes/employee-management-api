ac9e399cc194a39a5eda0aaa7128e4b6a527a059
Igor Gissi <gissiigor@gmail.com>
2026-09-11 10:06:42 -0300

    fix: test are not included in build and vitest only discovers tests in the correct folder

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,5 +6,6 @@ export default defineConfig({
         globals: true,
         setupFiles: ['./src/tests/setup.ts'],
         fileParallelism: false,
+        include: ['./src/tests/**/*.test.ts'],
     },
 });
